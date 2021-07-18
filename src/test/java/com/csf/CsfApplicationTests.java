963e535579ea246d656b417845b374a2845349b0
Dang Quang Tuan <tuan@enjoyworks.co.kr>
2021-07-18 18:57:35 +0700

    Initial source code project

---
+++ src/test/java/com/csf/CsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
