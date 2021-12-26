5df69ab40e2fa44fdfc3ddb3a4e307e9c21f4bfb
Dang Quang Tuan <tuan@enjoyworks.co.kr>
2021-12-26 16:59:36 +0700

    Transfer menu coding.

--- csf-whoami/src/main/java/com/csf/whoami/adm/MyPage.java
+++ csf-whoami/src/main/java/com/csf/whoami/adm/MyPage.java
@@ -1,16 +1,11 @@
 package com.csf.whoami.adm;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.csf.base.constant.ConstantsParam;
 import com.csf.base.constant.ConstantsURL;
-import com.csf.base.domain.MenuVO;
-import com.csf.whoami.service.MenuService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,8 +13,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping(value = ConstantsURL.DASHBOARD_HOME)
 public class MyPage {
-
-    private final MenuService menuService;
 
     @GetMapping
     public ModelAndView index(ModelAndView model) {
@@ -29,8 +22,6 @@ public class MyPage {
 
     @GetMapping(value = ConstantsURL.URI_DASHBOARD_MENU)
     public ModelAndView menus(ModelAndView model) {
-        List<MenuVO> listMenu = menuService.getMenus();
-        model.addObject(ConstantsParam.DATAS, listMenu);
         model.setViewName(ConstantsURL.PAGE_MENU);
         return model;
     }
